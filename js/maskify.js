13be356a28a5d38444fa1226ef7a7d5b9946e56a
Gustavo Valentim <gustavosvalentim1@gmail.com>
2019-03-24 13:52:36 -0300

    initial

---
+++ js/maskify.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/**
+ * Create a mask to an input and bind the element to a keydown event.
+ * 
+ * @param {string} mask String used as the mask for the input
+ * @return void
+ */
+Element.prototype.maskify = function(mask) {
+    const KEYCODES = [
+        8,  // backspace
+        9,  // tab
+        13, // enter
+        16, // shift
+        17, // ctrl
+        18, // alt
+        19, // pause/break
+        20, // caps lock
+        32, // space
+    ];
+    const SPECIALCHARS = [
+        '(',
+        ')',
+        '.',
+        '*',
+        '[',
+        ']',
+        '{',
+        '}',
+        '-',
+        ' '
+    ];
+    const SPLITMASK = mask.split('');
+    
+    this.addEventListener('keydown', keydownEvent);
+
+    function NextMaskChar(currentIndex) {
+        currentIndex++;
+
+        if(SPECIALCHARS.includes(SPLITMASK[currentIndex])) return SPLITMASK[currentIndex];
+        else return '';
+    }
+    
+    /**
+     * Event binded to input that will be masked.
+     * @param {event} event - That parameter is passed by the addEventListener builtin of JavaScript.
+     * @param {string} mask - This is the mask passed in the maskify function.
+     */
+    function keydownEvent(event) {
+        event.preventDefault();
+        let targetValue = event.target.value;
+
+        if(KEYCODES.includes(event.keyCode)) {
+            switch(event.keyCode) {
+                case 8:
+                    event.target.value = targetValue.slice(0, targetValue.length - 1);
+                    return;
+                case 32:
+                    event.target.value += ' ';
+                    return;
+                default:
+                    return;
+            }
+        }
+    
+        if(SPECIALCHARS.includes(SPLITMASK[targetValue.length])) {
+            event.target.value += SPLITMASK[targetValue.length];
+
+            let nextChar = NextMaskChar(targetValue.length);
+            if(nextChar !== '') event.target.value += nextChar;
+        }
+    
+        event.target.value += event.key;
+    }
+}
